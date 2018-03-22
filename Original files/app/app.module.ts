468bff35495600d15355c5a909f311477ffad902
Meir Yeheskel <meir.yeheskel>
2018-03-23 01:55:43 +0200

    final

--- Original files/app/app.module.ts
+++ Original files/app/app.module.ts
@@ -3,17 +3,11 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { MainComponent } from './main/main.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    MainComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule
